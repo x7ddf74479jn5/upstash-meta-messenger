484aebaaee5a4a8e295212b7aedd052b934d237c
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-11-17 01:26:30 +0900

    ➕ add: next-auth

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly REDIS_URL: string;
     readonly PUSHER_SECRET: string;
+    readonly FACEBOOK_ID: string;
+    readonly FACEBOOK_SECRET: string;
+    readonly NEXTAUTH_SECRET: string;
   }
 }
